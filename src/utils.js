8d7b204de9829e78eda563f8f2ee2f0c8e1fb56d
Ryan M. Waite <ryanwaite28@gmail.com>
2018-09-26 12:06:05 -0400

    updated

--- src/utils.js
+++ src/utils.js
@@ -143,7 +143,7 @@ export function loadPlaces() {
       console.log('fetching venues...');
       let city = 'Silver Spring, MD';
       let query = 'Shopping';
-      var apiURL = 'https://api.foursquare.com/v2/venues/search?client_id=N1IAMKZUIK1AUHKRFGFBKPQ2YKDSBAKS4NTER5SYZN5CROR1&client_secret=4MKLXVLU2FGZQVRMAEDC15P0TFJGSCY3ZUYUZ0KHQQQLQ5R3&v=20130815%20&limit=33&near=' + city + '&query=' + query + '';
+      var apiURL = 'https://api.foursquare.com/v2/venues/search?client_id=N1IAMKZUIK1AUHKRFGFBKPQ2YKDSBAKS4NTER5SYZN5CROR1&client_secret=4MKLXVLU2FGZQVRMAEDC15P0TFJGSCY3ZUYUZ0KHQQQLQ5R3&v=20130815%20&limit=50&near=' + city + '&query=' + query + '';
       fetch(apiURL)
       .then(resp => resp.json())
       .then(json => {
